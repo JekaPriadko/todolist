df00b6e294e0d7399946ffd4fcc666f254c8aff5
JekaPryadkoWeb <pryadkooooo1995@gmail.com>
2023-08-09 03:23:18 +0300

    refactor list.ts

--- src/assets/ts/core/list/ListUser.ts
+++ src/assets/ts/core/list/ListUser.ts
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import {
   getDatabase,
   ref,
@@ -7,12 +5,10 @@ import {
   Database,
   set,
   get,
-  update,
   child,
 } from 'firebase/database';
 
-import { uid } from 'uid';
-
+import ListUserModal from './ListUserModal';
 import DataTasksUser from '../tasks/DataTasksUser';
 import { List } from '../../entity/list';
 
@@ -28,29 +24,11 @@ class ListUser {
 
   private listsData: Array<List> = [];
 
-  private listWrap: HTMLElement | null;
-
-  private listItems: NodeListOf<HTMLElement> | null;
-
-  private addListBtn: HTMLElement | null;
-
-  private addListModal: HTMLElement | null;
-
-  private addListForm: HTMLFormElement | null;
-
-  private addListColorField: HTMLInputElement | null;
-
-  private addListTitleInput: HTMLInputElement | null;
-
-  private addListModalClose: NodeListOf<HTMLElement> | null;
-
   private dataTask: DataTasksUser;
 
-  private activeInputColor = false;
+  private modalAddList: ListUserModal;
 
-  private statusModal: 'update' | 'create';
-
-  private updateListItemId: string | null;
+  private listBlock: HTMLElement | null;
 
   constructor(userId) {
     this.userId = userId;
@@ -58,25 +36,11 @@ class ListUser {
 
     this.dataTask = new DataTasksUser(this.userId);
 
-    this.listWrap = document.getElementById('lists-js');
-    this.addListBtn = document.getElementById('add-list');
-    this.addListModal = document.getElementById('add-list-modal');
-    this.addListForm = document.getElementById(
-      'add-list-form'
-    ) as HTMLFormElement;
+    this.modalAddList = new ListUserModal(this.userId, this.db, this.listsData);
 
-    this.addListModalClose = document.querySelectorAll('.close-add-list-js');
-    this.addListColorField = this.addListModal.querySelector(
-      '.modal__content-color'
-    );
-    this.addListTitleInput = this.addListModal.querySelector(
-      '.modal__content-title'
-    );
+    this.listBlock = document.getElementById('lists-js');
 
-    this.addListForm.addEventListener('submit', (e) => this.addList(e));
-
-    this.handleFocusInputColor();
-    this.handleModalAddList();
+    this.setupListeners();
   }
 
   public async run() {
@@ -99,11 +63,18 @@ class ListUser {
     // ================================================================
 
     this.readyResolver();
+  }
 
+  private setupListeners() {
     document.addEventListener('changedTask', async () => {
       this.getOnceDataList();
     });
   }
+
+  public isReadyList() {
+    return this.readyPromise;
+  }
+
   private async getOnceDataList() {
     await get(child(ref(this.db), `${this.userId}/lists`))
       .then(async (snapshot) => {
@@ -128,128 +99,51 @@ class ListUser {
       .map((list) => this.renderLists(list))
       .join('');
 
-    this.listWrap.innerHTML = htmlList;
+    this.listBlock.innerHTML = htmlList;
     this.handleClickListItem();
-  }
 
-  public isReadyList() {
-    return this.readyPromise;
-  }
-
-  private handleModalAddList() {
-    this.addListBtn.addEventListener('click', async () => {
-      this.statusModal = 'create';
-      this.setupDataModal();
-      this.showAddModal();
-    });
-
-    this.addListModalClose.forEach((item: Element) => {
-      item.addEventListener('click', async () => {
-        this.closeModal();
-      });
-    });
-  }
-
-  private handleFocusInputColor() {
-    this.addListColorField.addEventListener('focus', () => {
-      this.activeInputColor = true;
-    });
-    this.addListColorField.addEventListener('blur', () => {
-      setTimeout(() => {
-        this.activeInputColor = false;
-      }, 100);
-    });
+    this.modalAddList.updateListsData(this.listsData);
   }
 
   private handleClickListItem() {
-    this.listItems = this.listWrap.querySelectorAll('.list-item-js');
+    const listItems: NodeListOf<HTMLElement> | null =
+      this.listBlock.querySelectorAll('.list-item-js');
 
-    this.listItems.forEach((item: Element) => {
+    listItems.forEach((item: Element) => {
       item.addEventListener('click', async (e) => {
         const targetElement = e.target as HTMLElement;
         const listId = item.getAttribute('data-id');
 
         const isEdit = targetElement.closest('.sidebar__lists-edit');
         if (isEdit) {
-          this.statusModal = 'update';
-          this.setupDataModal(
+          this.modalAddList.setStatusModal('update');
+          this.modalAddList.setupDataModal(
             this.listsData.find((elem) => elem.id === listId)
           );
-          this.showAddModal();
+          this.modalAddList.showAddModal();
           return;
         }
 
         const isDelete = targetElement.closest('.sidebar__lists-delete');
         if (isDelete) {
-          this.deleteList(listId);
+          this.deleteListItem(listId);
           return;
         }
 
-        console.log('filter');
+        console.log('make filter');
       });
     });
   }
 
-  private addList(e: SubmitEvent): void {
-    e.preventDefault();
-    const target = e.target as HTMLFormElement;
-    const formData = new FormData(target);
-
-    const newItem: List = {
-      title: formData.get('list-title') as string,
-      color: formData.get('list-color') as string,
-    };
-
-    if (this.statusModal === 'create') {
-      newItem.id = uid();
-      this.listsData.push(newItem);
-
-      set(
-        ref(this.db, `${this.userId}/lists`),
-        this.listsData.filter((item) => item !== null)
-      );
-    }
-
-    if (this.statusModal === 'update') {
-      newItem.id = this.updateListItemId;
-      const itemIndex = this.listsData.findIndex(
-        (item) => item.id === this.updateListItemId
-      );
-
-      const updates = {};
-      updates[`${this.userId}/lists/${itemIndex}`] = newItem;
-
-      update(ref(this.db), updates);
-    }
-
-    document.dispatchEvent(new Event('changedList'));
-    this.closeModal();
-  }
-
-  private deleteList(listId: string) {
+  private deleteListItem(listId: string) {
     set(
       ref(this.db, `${this.userId}/lists`),
       this.listsData.filter((item) => item !== null && item.id !== listId)
     );
   }
 
-  private closeModal() {
-    if (this.activeInputColor) return;
-
-    this.addListModal.classList.remove('active');
-  }
-
-  private setupDataModal(list?: List) {
-    this.addListColorField.value = list ? list.color : '#4772fa';
-    this.addListTitleInput.value = list ? list.title : '';
-    this.updateListItemId = list ? list.id : null;
-  }
-
-  private showAddModal() {
-    this.addListModal.classList.add('active');
-  }
-
   private renderLists(list: List): string {
+    /* eslint-disable */
     return `<li class="sidebar__lists-item list-item-js" data-id="${list.id}">
       <div class="sidebar__lists-button">
         <span class="sidebar__lists-name">${list.title}</span>
@@ -275,13 +169,14 @@ class ListUser {
         <div>
       </div>
     </li>`;
+    /* eslint-enable */
   }
 
   public static getLists(listUserInstance): Array<List> {
     return listUserInstance.listsData;
   }
 
-  public static getOneLists(listUserInstance: ListUser, listId): List {
+  public static getOneLists(listUserInstance: ListUser, listId: string): List {
     return (
       listUserInstance.listsData.find((item) => item.id === listId) || null
     );
---
+++ src/assets/ts/core/list/ListUserModal.ts
@@ -0,0 +1,148 @@
+import { ref, set, update, Database } from 'firebase/database';
+
+import { uid } from 'uid';
+
+import { List } from '../../entity/list';
+
+type PossibleModalStatus = 'update' | 'create';
+
+class ListUserModal {
+  private db: Database;
+
+  private userId: string | null;
+
+  private listsData: Array<List> = [];
+
+  private modal: HTMLElement | null;
+
+  private modalForm: HTMLFormElement | null;
+
+  private modalFormColorField: HTMLInputElement | null;
+
+  private modalFormTitleField: HTMLInputElement | null;
+
+  private modalCloseBtn: NodeListOf<HTMLElement> | null;
+
+  private isActiveInputColor = false;
+
+  private statusModal: PossibleModalStatus;
+
+  private updateListItemId: string | null;
+
+  constructor(userId: string | null, db: Database, listsData: Array<List>) {
+    this.userId = userId;
+    this.db = db;
+    this.listsData = listsData;
+
+    this.modal = document.getElementById('add-list-modal');
+    this.modalCloseBtn = document.querySelectorAll('.close-add-list-js');
+
+    this.modalForm = document.getElementById(
+      'add-list-form'
+    ) as HTMLFormElement;
+    this.modalFormColorField = this.modalForm.querySelector(
+      '.modal__content-color'
+    );
+    this.modalFormTitleField = this.modalForm.querySelector(
+      '.modal__content-title'
+    );
+
+    this.setupListeners();
+  }
+
+  private setupListeners() {
+    this.modalForm.addEventListener('submit', (e) => this.addList(e));
+    this.handleActiveInputColor();
+    this.handleModalShow();
+  }
+
+  public updateListsData(newListsData: Array<List>) {
+    this.listsData = newListsData;
+  }
+
+  private handleModalShow() {
+    const showListBtn = document.getElementById('show-modal-list');
+
+    showListBtn.addEventListener('click', async () => {
+      this.statusModal = 'create';
+      this.setupDataModal();
+      this.showAddModal();
+    });
+
+    this.modalCloseBtn.forEach((item: Element) => {
+      item.addEventListener('click', async () => {
+        this.closeModal();
+      });
+    });
+  }
+
+  private handleActiveInputColor() {
+    this.modalFormColorField.addEventListener('focus', () => {
+      this.isActiveInputColor = true;
+    });
+    this.modalFormColorField.addEventListener('blur', () => {
+      setTimeout(() => {
+        this.isActiveInputColor = false;
+      }, 100);
+    });
+  }
+
+  public showAddModal() {
+    console.log(this.listsData);
+    this.modal.classList.add('active');
+  }
+
+  public closeModal() {
+    if (this.isActiveInputColor) return;
+
+    this.modal.classList.remove('active');
+  }
+
+  public setupDataModal(list?: List) {
+    this.modalFormColorField.value = list ? list.color : '#4772fa';
+    this.modalFormTitleField.value = list ? list.title : '';
+    this.updateListItemId = list ? list.id : null;
+  }
+
+  public setStatusModal(newStatus: PossibleModalStatus) {
+    this.statusModal = newStatus;
+  }
+
+  private addList(e: SubmitEvent): void {
+    e.preventDefault();
+    const target = e.target as HTMLFormElement;
+    const formData = new FormData(target);
+
+    const newItem: List = {
+      title: formData.get('list-title') as string,
+      color: formData.get('list-color') as string,
+    };
+
+    if (this.statusModal === 'create') {
+      newItem.id = uid();
+      this.listsData.push(newItem);
+
+      set(
+        ref(this.db, `${this.userId}/lists`),
+        this.listsData.filter((item) => item !== null)
+      );
+    }
+
+    if (this.statusModal === 'update') {
+      newItem.id = this.updateListItemId;
+      const itemIndex = this.listsData.findIndex(
+        (item) => item.id === this.updateListItemId
+      );
+
+      const updates = {};
+      updates[`${this.userId}/lists/${itemIndex}`] = newItem;
+
+      update(ref(this.db), updates);
+    }
+
+    document.dispatchEvent(new Event('changedList'));
+    this.closeModal();
+  }
+}
+
+export default ListUserModal;
